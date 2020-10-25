735199a14d0c55fb20488ddac271d1397cf7ed04
Camille Masset <masset.camille@gmail.com>
2020-10-25 18:44:02 +0100

    Simplify setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-SKIP_CLONE={$SKIP_CLONE:-0}
-
 function check-command() {
     if ! command -v "$1" >/dev/null 2>&1; then
         echo "Command $1 not found. Aborting installation."
@@ -9,33 +7,14 @@ function check-command() {
     fi
 }
 
-check-command git
 check-command brew
+check-command git || brew install git
 
-exit 0
-
-# ---------------------------------------------------------------------------- #
-#                                   BOOTSTRAP                                  #
-# ---------------------------------------------------------------------------- #
-
-if [ $SKIP_CLONE == 0 ]; then
-    REPO_NAME="macos-setup"
-    REPO_PARENT_DIR="$HOME/.shell-utils.tmp" # TODO: remove .tmp
-    REPO_LOC="$REPO_PARENT_DIR/$REPO_NAME"
-
-    mkdir -p "$REPO_PARENT_DIR"
-    git clone "git@github.com:tizot/$REPO_NAME.git" "$REPO_LOC"
-
-    cd "$REPO_LOC" || exit 1
-fi
-
+# Install dependencies
 brew install ansible
 ansible-galaxy install -r requirements.yml
 
-# ---------------------------------------------------------------------------- #
-#                             INSTALL APPLICATIONS                             #
-# ---------------------------------------------------------------------------- #
-
+# Setup system
 ansible-playbook -i inventory --ask-become-pass playbook.yml
 
 exit 0
